428f630e95a8d072135d999b39726587234b7381
Lars Birger <larsbirger.bergmaal@gmail.com>
2021-03-17 01:00:44 +0100

    added library zip (may need link for where i got the library from) and updated the Ambient light sensor to utilise the library

--- Ambient_Light/Ambient_Light.ino
+++ Ambient_Light/Ambient_Light.ino
@@ -1,9 +1,24 @@
+#include <Wire.h>
+#include <BH1750.h>
+
+const int sensor_address	= 0x23; //address
+
+const int FREQ = 74880;
+
+BH1750 lightMeter(sensor_address);
+
 void setup()
 {
-	
+    Serial.begin(FREQ);
+    Wire.begin();
+    lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE_2);
 }
 
 void loop()
 {
-	
+    if (lightMeter.measurementReady())
+    {
+        uint16_t lux = lightMeter.readLightLevel();
+        Serial.println(lux);
+    }
 }
